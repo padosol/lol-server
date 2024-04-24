97aca1201efa5a1e36584d71c2d6a3098d5f42b7
한상훈 <hzyan2@gmail.com>
2024-04-24 22:29:19 +0900

    유저검색 로그 추가

--- src/main/java/com/example/lolserver/web/summoner/controller/SummonerV2Controller.java
+++ src/main/java/com/example/lolserver/web/summoner/controller/SummonerV2Controller.java
@@ -5,6 +5,7 @@ import com.example.lolserver.web.summoner.dto.SummonerResponse;
 import com.example.lolserver.web.summoner.entity.Summoner;
 import com.example.lolserver.web.summoner.service.SummonerService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -32,10 +35,21 @@ public class SummonerV2Controller {
             @RequestParam("q") String q, @RequestParam("region") String region
     ) throws IOException, InterruptedException {
 
+        log.info("getAllSummoner start");
+
         List<SummonerResponse> result = summonerServiceV2.getAllSummoner(Summoner.builder()
                 .name(q)
                 .region(region)
                 .build());
+
+        for (SummonerResponse summonerResponse : result) {
+            log.info("Gamename: {}", summonerResponse.getGameName());
+            log.info("Tagline: {}", summonerResponse.getTagLine());
+        }
+
+
+
+        log.info("getAllSummoner end");
 
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
