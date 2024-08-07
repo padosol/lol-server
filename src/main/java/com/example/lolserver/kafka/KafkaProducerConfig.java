a60af3cc849393a60d811feab05b5612cc8cb3f9
한상훈 <hzyan2@gmail.com>
2024-08-07 21:18:59 +0900

    kafka 서버 properties 설정

--- src/main/java/com/example/lolserver/kafka/KafkaProducerConfig.java
+++ src/main/java/com/example/lolserver/kafka/KafkaProducerConfig.java
@@ -3,6 +3,7 @@ package com.example.lolserver.kafka;
 import com.example.lolserver.web.summoner.entity.Summoner;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -16,11 +17,14 @@ import java.util.Map;
 @Configuration
 public class KafkaProducerConfig {
 
+    @Value("${spring.kafka.producer.bootstrap-servers}")
+    private String boostrapServer;
+
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, boostrapServer);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         config.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, "10485880"); // 10MB
