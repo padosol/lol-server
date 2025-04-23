89351fb21907532bfbb41f0dec9033e7be658d1d
한상훈 <hzyan2@gmail.com>
2025-04-23 12:30:24 +0900

    remove: kafka 제거

--- src/main/java/com/example/lolserver/web/match/entity/id/MatchSummonerId.java
+++ src/main/java/com/example/lolserver/web/match/entity/id/MatchSummonerId.java
@@ -1,7 +1,6 @@
 package com.example.lolserver.web.match.entity.id;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
