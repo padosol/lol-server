1f26a31fcc5345903a46811459188ddf6b151227
한상훈 <hzyan2@gmail.com>
2025-12-01 13:32:21 +0900

    refactor: 매치 정보에서 받은 패해량 추가

--- core/core-api/src/main/java/com/example/lolserver/domain/match/service/MatchServiceImpl.java
+++ core/core-api/src/main/java/com/example/lolserver/domain/match/service/MatchServiceImpl.java
@@ -54,14 +54,6 @@ public class MatchServiceImpl implements MatchService {
         );
     }
 
-//    @Override
-//    public List<MSChampionDTO> getRankChampions(MSChampionRequest request) {
-//        return matchSummonerRepositoryCustom.findAllChampionKDAByPuuidAndSeasonAndQueueType(
-//                request.getPuuid(),
-//                request.getSeason()
-//        );
-//    }
-
     @Override
     public GameData getGameData(String matchId) {
         Match match = matchRepository.findById(matchId).orElseThrow(() -> new CoreException(
